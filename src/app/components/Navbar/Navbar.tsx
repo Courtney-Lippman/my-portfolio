2bac5ed607072fe3eca28159ba11e62ad903bcf6
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-15 13:49:53 -0600

    Add Navbar and Main components and then add basic navbar styling to Navbar component.

---
+++ src/app/components/Navbar/Navbar.tsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    return(
+        <nav
+        className="
+          flex flex-wrap
+          items-center
+          justify-between
+          w-full
+          py-4
+          md:py-0
+          px-4
+          text-lg text-gray-700
+          bg-white
+        "
+      >
+      {/* code goes here */}
+    </nav>
+    )
+}
+
+export default Navbar
