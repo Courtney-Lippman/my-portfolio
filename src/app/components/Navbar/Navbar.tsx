d47ea9d72d129e6fe2712d01b5ca0b54218905f7
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-15 22:41:54 -0600

    Change tailwind layout to include grid as parent and flex for nav children.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,13 +1,15 @@
+import './Navbar.css'
+
 const Navbar = () => {
     return(
     <nav
-    className="flex justify-center flex-row w-1/2"
+    className="grid grid-cols-2 gap-x-8 w-screen h-1/3"
     >
-        <p className="text-2xl flex justify-left w-1/2">C. Lippman</p>
-        <div className="nav-wrapper flex justify-right w-1/2">
-        <p className="pl-8">Work</p>
-        <p className="pl-8">About</p>
-        <p className="pl-8">Contact</p>
+        <p className="logo flex justify-center">C. Lippman</p>
+        <div className="nav-wrapper flex justify-center">
+            <p className="p-8 text-5xl">Work</p>
+            <p className="p-8 text-5xl">About</p>
+            <p className="p-8 text-5xl">Contact</p>
         </div>
     </nav>
     )
