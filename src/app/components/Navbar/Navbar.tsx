c249a7e4f5f5b7a2072d1734c5f3a8dadf37942e
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-15 20:48:19 -0600

    Add flex layout to Navbar elements.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,19 +1,14 @@
 const Navbar = () => {
     return(
-        <nav
-        className="
-          flex flex-wrap
-          items-center
-          justify-between
-          w-full
-          py-4
-          md:py-0
-          px-4
-          text-lg text-gray-700
-          bg-white
-        "
-      >
-      {/* code goes here */}
+    <nav
+    className="flex justify-center flex-row w-1/2"
+    >
+        <p className="text-2xl flex justify-left w-1/2">C. Lippman</p>
+        <div className="nav-wrapper flex justify-right w-1/2">
+        <p className="pl-8">Work</p>
+        <p className="pl-8">About</p>
+        <p className="pl-8">Contact</p>
+        </div>
     </nav>
     )
 }
