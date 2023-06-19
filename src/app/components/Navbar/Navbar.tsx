85b8cb1ebd9000e42dc7444ffc9c69d157d62da5
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-19 15:12:14 -0600

    Change brand logo to all lowercase.

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -5,11 +5,11 @@ const Navbar = () => {
     <nav
     className="grid grid-cols-2 gap-x-8 w-screen h-1/3"
     >
-        <p className="logo flex justify-center sm:text-5xl md:text-6xl lg:text-7xl xl:text-8xl">C. Lippman</p>
-        <div className="nav-wrapper flex justify-center">
-            <p className="p-8 sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl ">Work</p>
-            <p className="p-8 sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl">About</p>
-            <p className="p-8 sm:text-1xl md:text-2xl lg:text-4xl xl:text-5xl">Contact</p>
+        <p className="logo flex justify-center items-end sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl">c. lippman</p>
+        <div className="nav-wrapper flex justify-center items-end">
+            <p className="px-8 sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl ">Work</p>
+            <p className="px-8 sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl">About</p>
+            <p className="px-8 sm:text-1xl md:text-2xl lg:text-3xl xl:text-4xl">Contact</p>
         </div>
     </nav>
     )
