66901337e818fa6df3b1dcc9689eca6f20b552f4
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-22 14:13:15 -0600

    Add project objects to project array.

--- src/app/components/WorkSection/WorkSection.tsx
+++ src/app/components/WorkSection/WorkSection.tsx
@@ -1,7 +1,40 @@
 import "./WorkSection.css";
 
 const WorkSection = () => {
-  return <section></section>;
+  const projects = [
+    {
+      title: "Streamr",
+      image: "Hello;",
+      link: "https://github.com/streamr-turing/streamr-fe"
+    },
+    {
+      title: "Board Game Bonanza",
+      image: "Hello;",
+      link: "https://github.com/Courtney-Lippman/board-game-bonanza"
+    },
+    {
+      title: "What's Cookin Good Lookin",
+      image: "Hello;",
+      link: "https://github.com/Courtney-Lippman/whats-cookin-good-lookin"
+    },
+    {
+      title: "Sip and Read",
+      image: "Hello;",
+      link: "https://github.com/Courtney-Lippman/sip-and-read"
+    },
+    {
+      title: "Pomodori Putridi",
+      image: "Hello;",
+      link: "https://github.com/Courtney-Lippman/sip-and-read"
+    }
+  ];
+
+  return (
+    <section>
+      <h1>Recent Projects</h1>
+      <section className="grid"></section>
+    </section>
+  );
 };
 
 export default WorkSection;
