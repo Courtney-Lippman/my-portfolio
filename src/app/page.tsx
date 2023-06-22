50ae949a37cab93614dfef49ceea050b765df620
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-22 13:40:12 -0600

    Add WorkSection component to page render.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
-import Image from 'next/image'
-import Navbar from './components/Navbar/Navbar'
-import Carousel from './components/Carousel/Carousel'
-
+import Image from "next/image";
+import Navbar from "./components/Navbar/Navbar";
+import Carousel from "./components/Carousel/Carousel";
+import WorkSection from "./components/WorkSection/WorkSection";
 
 export default function Home() {
   return (
     <main className=" w-screen min-h-screen sm:pt-3 md:pt-4 lg:pt-5 xl:pt-6 box-border flex flex-col items-center">
       <Navbar />
       <Carousel />
+      <WorkSection />
     </main>
-  )
+  );
 }
