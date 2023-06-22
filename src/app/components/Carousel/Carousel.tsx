fffdd20e846ac089bed344a049a7dd7467a80968
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-22 13:09:26 -0600

    Move the text overlay to be centered via x axis, also center arrow buttons, and align all text to center.

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -1,26 +1,27 @@
 "use client";
-import React, { useState } from 'react'
-import './Carousel.css'
-import { BsChevronCompactLeft, BsChevronCompactRight } from 'react-icons/bs'
-import { RxDotFilled } from 'react-icons/rx'
+import React, { useState } from "react";
+import "./Carousel.css";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+import { RxDotFilled } from "react-icons/rx";
 
 const Carousel = () => {
-
   const slides = [
     {
-      url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-      message1: 'Hello;',
-      message2: 'I\'m Courtney Lippman. Front-end Engineer, React and Typescript Developer.'
+      url: "https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80",
+      message1: "Hello;",
+      message2:
+        "I'm Courtney Lippman. Front-end Engineer, React and Typescript Developer."
     },
     {
-      url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-      message1: 'From caring for individuals to impacting communities.',
-      message2: ''
+      url: "https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80",
+      message1: "From caring for individuals to impacting communities.",
+      message2: ""
     },
     {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-      message1: 'To create meaningful solutions that scale and make a difference.',
-      message2: ''
+      url: "https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80",
+      message1:
+        "To create meaningful solutions that scale and make a difference.",
+      message2: ""
     }
   ];
 
@@ -42,41 +43,43 @@ const Carousel = () => {
     setCurrentIndex(slideIndex);
   };
 
-    return(
-        <div className='max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group'>
-        <div
-          style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
-          className='w-full h-full rounded-2xl bg-center bg-cover duration-500 relative'
-        >
-              <div className="absolute bottom-0 px-4 py-3 bg-gray-500/50 w-full">
-    <h1 className="text-white font-semibold text-4xl">{slides[currentIndex].message1}</h1>
-    <p className="text-gray-200">
-    {slides[currentIndex].message2}
-    </p>
-  </div>
-        </div>
-        {/* Left Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactLeft onClick={prevSlide} size={30} />
-        </div>
-        {/* Right Arrow */}
-        <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
-          <BsChevronCompactRight onClick={nextSlide} size={30} />
-        </div>
-        {/* Dots */}
-        <div className='flex top-4 justify-center py-2'>
-          {slides.map((slide, slideIndex) => (
-            <div
-              key={slideIndex}
-              onClick={() => goToSlide(slideIndex)}
-              className='text-2xl cursor-pointer '
-            >
-              <RxDotFilled />
-            </div>
-          ))}
+  return (
+    <div className="max-w-[1400px] h-[780px] w-full m-auto py-16 px-4 relative group">
+      <div
+        style={{ backgroundImage: `url(${slides[currentIndex].url})` }}
+        className="w-full h-full rounded-2xl bg-center bg-cover duration-500 relative"
+      >
+        <div className="absolute top-1/2 px-4 pybg-gray-500/50 w-full flex flex-col justify-centitems-center">
+          <h1 className="text-white font-semibold text-4xl text-center">
+            {slides[currentIndex].message1}
+          </h1>
+          <p className="text-gray-200 text-center">
+            {slides[currentIndex].message2}
+          </p>
         </div>
       </div>
-    )
-}
+      {/* Left Arrow */}
+      <div className="hidden group-hover:block absolute top-1/2 left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactLeft onClick={prevSlide} size={30} />
+      </div>
+      {/* Right Arrow */}
+      <div className="hidden group-hover:block absolute top-1/2 right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer">
+        <BsChevronCompactRight onClick={nextSlide} size={30} />
+      </div>
+      {/* Dots */}
+      <div className="flex top-4 justify-center py-2">
+        {slides.map((slide, slideIndex) => (
+          <div
+            key={slideIndex}
+            onClick={() => goToSlide(slideIndex)}
+            className="text-2xl cursor-pointer "
+          >
+            <RxDotFilled />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
-export default Carousel
+export default Carousel;
