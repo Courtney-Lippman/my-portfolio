0be0b8532a7bb01578a66183daaf71ba4bbd15c8
Courtney Lippman <courtney.lippman@colorado.edu>
2023-06-21 21:36:23 -0600

    Add new messages.

--- src/app/components/Carousel/Carousel.tsx
+++ src/app/components/Carousel/Carousel.tsx
@@ -9,30 +9,19 @@ const Carousel = () => {
   const slides = [
     {
       url: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2620&q=80',
-      message1: 'Adios!',
-      message2: 'Hm'
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2670&q=80',
-      message1: 'Chao!',
-      message2: 'Bee'
-    },
-    {
-      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
-      message1: 'Hello!',
-      message2: 'Too'
-    },
-
-    {
-      url: 'https://images.unsplash.com/photo-1512756290469-ec264b7fbf87?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2253&q=80',
-      message1: 'Hi!',
-      message2: 'Very'
+      message1: 'Hello;',
+      message2: 'I\'m Courtney Lippman. Front-end Engineer, React and Typescript Developer.'
     },
     {
       url: 'https://images.unsplash.com/photo-1496181133206-80ce9b88a853?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2671&q=80',
-      message1: 'Bye!',
-      message2: 'Sweet'
+      message1: 'My Mission:',
+      message2: 'From caring for individuals to impacting communities, I\'ve shifted my focus to software engineering to create meaningful solutions that scale and make a difference.'
     },
+    {
+      url: 'https://images.unsplash.com/photo-1661961112951-f2bfd1f253ce?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2672&q=80',
+      message1: 'Who I am:',
+      message2: 'Curious, Driven, Dreamer.'
+    }
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -74,12 +63,13 @@ const Carousel = () => {
         <div className='hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
           <BsChevronCompactRight onClick={nextSlide} size={30} />
         </div>
+        {/* Dots */}
         <div className='flex top-4 justify-center py-2'>
           {slides.map((slide, slideIndex) => (
             <div
               key={slideIndex}
               onClick={() => goToSlide(slideIndex)}
-              className='text-2xl cursor-pointer'
+              className='text-2xl cursor-pointer '
             >
               <RxDotFilled />
             </div>
