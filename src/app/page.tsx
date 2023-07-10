71f4f7f5394d101810cfbec40da1450c9109ddaa
Courtney Lippman <courtney.lippman@colorado.edu>
2023-07-10 13:12:31 -0600

    Add useState for navToPage state and pass as prop to Navbar component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
+"use client";
 import Image from "next/image";
 import Navbar from "./components/Navbar/Navbar";
 import Carousel from "./components/Carousel/Carousel";
 import WorkSection from "./components/WorkSection/WorkSection";
+import { useState } from "react";
 
 export default function Home() {
+  const [navToPage, setNavToPage] = useState<string>("home");
+
   return (
-    <main className=" w-screen min-h-screen sm:pt-3 md:pt-4 lg:pt-5 xl:pt-6 box-border flex flex-col items-center">
-      <Navbar />
+    <main className="w-screen min-h-screen sm:pt-3 md:pt-4 lg:pt-5 xl:pt-6 box-border flex flex-col items-center">
+      <Navbar setNavToPage={setNavToPage} />
       <Carousel />
       <WorkSection />
     </main>
