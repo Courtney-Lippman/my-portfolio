9da1a05a45fae4882ef4fcb54f93c9fef2ab92e1
Courtney Lippman <courtney.lippman@colorado.edu>
2023-07-10 13:47:05 -0600

    Add conditional rendering for when navToPage state is equal to home.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,8 +11,8 @@ export default function Home() {
   return (
     <main className="w-screen min-h-screen sm:pt-3 md:pt-4 lg:pt-5 xl:pt-6 box-border flex flex-col items-center">
       <Navbar setNavToPage={setNavToPage} />
-      <Carousel />
-      <WorkSection />
+      {navToPage === "home" && <Carousel />}
+      {navToPage === "home" && <WorkSection />}
     </main>
   );
 }
