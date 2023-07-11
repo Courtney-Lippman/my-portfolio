7e836d09d3c76a2bb6d32dd7d040339b78d67d6d
Courtney Lippman <courtney.lippman@colorado.edu>
2023-07-10 23:03:51 -0600

    Change DeskTop component to house logic for changing the navToPage state based on the button clicked.

--- src/app/components/Navbar/DesktopMenu/DeskTopMenu.tsx
+++ src/app/components/Navbar/DesktopMenu/DeskTopMenu.tsx
@@ -1,12 +1,43 @@
-"use client";
-import { useState } from "react";
+import React, { MouseEvent, useCallback } from "react";
 
-const DeskTopMenu = () => {
+interface DeskTopMenuProps {
+  setNavToPage: (value: string) => void;
+}
+
+const DeskTopMenu: React.FC<DeskTopMenuProps> = ({ setNavToPage }) => {
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLDivElement>) => {
+      const target = event.target as HTMLParagraphElement;
+      const value = target.getAttribute("data-value");
+      if (value) {
+        setNavToPage(value);
+      }
+    },
+    [setNavToPage]
+  );
+
   return (
     <div className="nav-wrapper justify-center items-end hidden md:flex">
-      <p className="px-8 md:text-2xl lg:text-3xl xl:text-4xl ">Work</p>
-      <p className="px-8  md:text-2xl lg:text-3xl xl:text-4xl">About</p>
-      <p className="px-8 md:text-2xl lg:text-3xl xl:text-4xl">Contact</p>
+      <a
+        className="px-8 md:text-2xl lg:text-3xl xl:text-4xl"
+        href="#work-section"
+      >
+        Work
+      </a>
+      <p
+        className="px-8 md:text-2xl lg:text-3xl xl:text-4xl"
+        onClick={handleClick}
+        data-value="about-button"
+      >
+        About
+      </p>
+      <p
+        className="px-8 md:text-2xl lg:text-3xl xl:text-4xl"
+        onClick={handleClick}
+        data-value="contact-button"
+      >
+        Contact
+      </p>
     </div>
   );
 };
