69f4a07e80b4900e2c96cb5ee98bd41d9359083b
Courtney Lippman <courtney.lippman@colorado.edu>
2023-07-15 18:03:31 -0600

    Add react icons for GitHub and LinkedIn on Contact page.

--- src/app/contact/ContactSection/ContactSection.tsx
+++ src/app/contact/ContactSection/ContactSection.tsx
@@ -1,7 +1,11 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+
 export default function ContactSection() {
   return (
     <main>
-      <h1>Contact Me</h1>
+      <h1>Let's Connect</h1>
+      <BsGithub />
+      <BsLinkedin />
     </main>
   );
 }
