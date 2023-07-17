8cecd9a6108ec071b89d192dc12ecdc0f76ac652
Courtney Lippman <courtney.lippman@colorado.edu>
2023-07-17 09:48:54 -0600

    Add classnames to jsx elements for ContactSection.

--- src/app/contact/ContactSection/ContactSection.tsx
+++ src/app/contact/ContactSection/ContactSection.tsx
@@ -2,10 +2,10 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 export default function ContactSection() {
   return (
-    <main>
+    <main className="w-100 h-100">
       <h1>Let's Connect</h1>
-      <BsGithub />
-      <BsLinkedin />
+      <BsGithub className="" />
+      <BsLinkedin className="" />
     </main>
   );
 }
